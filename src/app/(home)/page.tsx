77d77e943f9e496d8e27f1f1e558555ba3ef6f62
Temesgen Tsegaye <temesgentsegaye9891@gmail.com>
2024-05-23 22:23:20 +0300

    final

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,8 +1,11 @@
+"use client"
 import Image from "next/image";
 
 import { Box } from "@mui/material";
+import { useRouter } from "next/navigation";
 export default async  function Home() {
-
+ const router=useRouter()
+ router.push('/dashboard')
   return (
    <>
    </>
--- src/auth/index.ts
+++ src/auth/index.ts
@@ -1,6 +1,6 @@
 import NextAuth, { User, NextAuthConfig } from "next-auth";
 import Credentials from "next-auth/providers/credentials";
-
+import { prisma } from "@/config/prisma-client";
 export const BASE_PATH = "/api/auth";
 
 const authOptions: NextAuthConfig = {
@@ -11,30 +11,17 @@ const authOptions: NextAuthConfig = {
         phoneNumber: { label: "phoneNumber", type: "text", placeholder: "jsmith" },
         password: { label: "Password", type: "password" },
       },
-      async authorize(credentials): Promise<User | null> {
-        const users = [
-          {
-            id: "test-user-1",
-            userName: "test1",
-            name: "Test 1",
-            password: "pass",
-            email: "test1@donotreply.com",
-          },
-          {
-            id: "test-user-2",
-            userName: "test2",
-            name: "Test 2",
-            password: "pass",
-            email: "test2@donotreply.com",
-          },
-        ];
-        const user = users.find(
-          (user) =>
-            user.userName === credentials.phoneNumber &&
-            user.password === credentials.password
-        );
+      async authorize(credentials): Promise<any> {
+        
+
+        const user=await prisma.user.findUnique({
+          where:{
+            phonenumber:credentials.phoneNumber as string
+          }
+        })
+       
         return user
-          ? { id: user.id, name: user.name, email: user.email }
+          ? { id: user.id, name: user.name, email:user.email  }
           : null;
       },
     }),
