bafaf04464744bb13a95215c65ee923f95199773
Temesgen Tsegaye <temesgentsegaye9891@gmail.com>
2024-05-23 22:36:44 +0300

    a

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 
 import { Box } from "@mui/material";
 import { useRouter } from "next/navigation";
-export default async  function Home() {
+export default  function Home() {
  const router=useRouter()
  router.push('/dashboard')
   return (
