0b15872628031a73d303268d5120ae6a8e9ef432
Temesgen Tsegaye <temesgentsegaye9891@gmail.com>
2024-06-19 21:56:08 +0300

    working with prisma

--- src/app/(home)/dashboard/component/pi_chart.tsx
+++ src/app/(home)/dashboard/component/pi_chart.tsx
@@ -2,139 +2,47 @@
 
 import React from "react";
 import { Box, Paper, Typography } from "@mui/material";
-import { VictoryPie, VictoryLabel } from "victory";
 import { Prisma } from "@prisma/client";
+import { socket } from "@/utils/socket/socket-client";
 
 
 
 
-const colorLegend = [
-  { name: "Recommended", color: "#8D1FB4" },
-  { name: "Popular", color: "#008000" },
-  { name: "Featured", color: "#1484E6" },
-  { name: "Favorite", color: "#16C138" },
-  { name: "Watch later", color: "#E10070" },
-];
 
-function convert(id:number){
-      if(id==1){
-        return  "Recommended"
-      }else if (id==2){
-        return "Popular"
-      }else if (id==3){
-      return  "Featured"
-      }else if(id==4){
-        return "Favorite"
-      }else if(id==5){
-        
-        return "Watch later"
-}}
+
 
 export default function PiChart({pie}:{pie:(Prisma.PickEnumerable<Prisma.ContentGroupByOutputType, "categoryId"[]> & {
   _count: {
       id: number;
   };
-})[]}) {
-  const mappedData = pie.map((item) => {
-    // Handle potential missing categoryId or non-numeric value
-    const categoryName = convert(item.categoryId)
-  
-    return {
-      ...item, 
-      categoryName,
+})[]}) 
+
+
+
+{
+
+   const [realCounts,setRealCounts]=React.useState(0)
+
+  React.useEffect(() => {
+    const handleAddChannel = (count: number) => {
+      console.log(count,'cou')
+      setRealCounts(count);
     };
-  }).map(item => ({
-    x: `Category ${item.categoryName}`, // Or you can map category IDs to their names if you have a mapping
-    y: item._count.id
-}));
+    socket.on('addChannel', handleAddChannel);
+  
+    return () => {
+      socket.off('addChannel', handleAddChannel);
+    };
+  }, []);
+ 
   return (
     <Paper
       elevation={3}
       sx={{ width: "60%", position: "relative", height: "20rem",display:"flex",justifyContent:"center",alignItems:"center" }}
     >
-      <Box
-        sx={{
-          borderRadius: "10px",
-          bgcolor: "black",
-          padding: "1rem",
-          width: "40%",
-          fontWeight: 600,
-          position: "absolute",
-          top: "-1.7rem",
-          left:0,
-          color: "white",
-          textAlign: "center",
-        }}
-      >
-        Program on Category
-      </Box>
-      <Box sx={{display:"flex",justifyContent:"center",alignItems:'center', width:"100%",height:"100%"}}>
-      <Box>
-        <Box style={{ width: "60%", paddingRight: "20px", display: "flex",position:"relative" }}>
-      
-          <VictoryPie
-            data={mappedData}
-            colorScale={colorLegend.map(item => item.color)}
-            labels={({ datum }) => ""}
-            labelComponent={<></>}
-            padAngle={5}
-            innerRadius={150}
-            width={500}
-            height={500}
-           
-
-          >
-
-          </VictoryPie>
-        </Box>
-      </Box>
-      <Box style={{ width: "50%" }}>
-  {colorLegend.map((item, index) => (
-    <Box
-      key={index}
-      style={{
-        display: "flex",
-        alignItems: "center",
-        marginBottom: "5px",
-      }}
-    >
-      <Box
-        style={{
-          width: "20px",
-          height: "20px",
-          backgroundColor: item.color,
-          marginRight: "5px",
-        }}
-      ></Box>
-      <Typography variant="body2" style={{ marginRight: "5px" }}>{item.name}</Typography>
-      <Typography variant="body2">{mappedData.find(data => data.x === `Category ${item.name}`)?.y || 0}</Typography>
-    </Box>
-  ))}
-</Box>
-      {/* <Box style={{ width: "50%" }}>
-        {colorLegend.map((item, index) => (
-          <Box
-            key={index}
-            style={{
-              display: "flex",
-              alignItems: "center",
-              marginBottom: "5px",
-            }}
-          >
-            <Box
-              style={{
-                width: "20px",
-                height: "20px",
-                backgroundColor: item.color,
-                marginRight: "5px",
-              }}
-            ></Box>
-            <span>{item.name}</span>
-          </Box>
-        ))}
-      </Box> */}
-      </Box>
-    
+     
+  
+  <div className="text-3xl">{realCounts}</div>
 
       
     </Paper>
